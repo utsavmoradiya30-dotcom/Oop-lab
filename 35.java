b8a818d3662c29a6969afb867608a40b6c513eb0
utsavmoradiya30-dotcom <utsavmoradiya30@gmail.com>
2026-04-30 10:12:41 +0530

    Create 35.java

---
+++ 35.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package utsav_240760107191;
+class BankAccount {
+    String name;
+    double balance;
+    static double interest_rate = 5; // shared
+
+    BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    double calculateInterest() {
+        return (balance * interest_rate) / 100;
+    }
+
+    void display() {
+        System.out.println(name + " Interest: " + calculateInterest());
+    }
+
+    static void setInterestRate(double rate) {
+        interest_rate = rate;
+    }
+}
+
+
+
+/**
+ *
+ * @author nikunjjoshi
+ */
+public class PS10_35{
+    
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         BankAccount b1 = new BankAccount("Tarun", 10000);
+        BankAccount b2 = new BankAccount("Raj", 20000);
+
+        b1.display();
+        b2.display();
+
+        // Update interest rate
+        BankAccount.setInterestRate(7);
+
+        System.out.println("\nAfter updating interest rate:");
+        b1.display();
+        b2.display();
+        
+        
+    }
+    
+}
